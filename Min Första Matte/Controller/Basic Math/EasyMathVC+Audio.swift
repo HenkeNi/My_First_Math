d007500c399a3aff4388e27028a4ed923c4d4b92
Henrik Jangefelt <henke9105@hotmail.com>
2020-01-07 01:06:03 +0100

    Used multiple collection outlets for views in easy math

---
+++ Min Första Matte/Controller/Basic Math/EasyMathVC+Audio.swift
@@ -0,0 +1,9 @@
+//
+//  EasyMathVC+Audio.swift
+//  Min Första Matte
+//
+//  Created by Henrik Jangefelt on 2020-01-06.
+//  Copyright © 2020 Henrik Jangefelt Nilsson. All rights reserved.
+//
+
+import Foundation

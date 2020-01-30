6de7f4620eba2b7f8c2fd331ee3d218bd90ee91c
Henrik Jangefelt <henke9105@hotmail.com>
2020-01-31 00:05:13 +0100

    Trying to remove UIKit from models (MVC)

---
+++ Min Första Matte/Model/BasicMath/cardPosition.swift
@@ -0,0 +1,9 @@
+//
+//  CardPosition.swift
+//  Min Första Matte
+//
+//  Created by Henrik Jangefelt on 2020-01-30.
+//  Copyright © 2020 Henrik Jangefelt Nilsson. All rights reserved.
+//
+
+import Foundation
